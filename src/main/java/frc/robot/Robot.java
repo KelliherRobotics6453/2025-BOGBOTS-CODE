65b2670edf600e948dec97028a5771de0f25081e
kedrickjohnson <38338857+kedrickjohnson@users.noreply.github.com>
2025-03-12 09:51:03 -0500

    Added Logging

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,10 @@
 
 package frc.robot;
 
+import org.littletonrobotics.urcl.URCL;
+
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -22,6 +26,10 @@ public class Robot extends TimedRobot {
 
   public Robot() {
     m_robotContainer = new RobotContainer();
+     DataLogManager.start();
+    DriverStation.startDataLog(DataLogManager.getLog(), true); 
+    URCL.start();
+
   }
 
   @Override
